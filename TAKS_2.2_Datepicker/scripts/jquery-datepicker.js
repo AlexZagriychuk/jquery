15d20e7c55a95e30f44f456d2ab87c7ddae93374
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-01 21:37:16 -0700

    Task 2.2 - datepicker - implemented displaying a button for selecting Today’s date and a Done button for closing the calendar; added slideDown aniamtion

--- TAKS_2.2_Datepicker/scripts/jquery-datepicker.js
+++ TAKS_2.2_Datepicker/scripts/jquery-datepicker.js
@@ -1,8 +1,32 @@
 $(function () {
   $("#datepicker").datepicker({
+    showAnim: "slideDown",
+    showButtonPanel: true,
     showOn: "both",
     buttonImage: "images/icons/calendar.gif",
     buttonImageOnly: true,
     buttonText: "Select date"
   })
 });
+
+
+$.datepicker._gotoToday = function (id) {
+  var target = $(id);
+  var inst = this._getInst(target[0]);
+  if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+    inst.selectedDay = inst.currentDay;
+    inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+    inst.drawYear = inst.selectedYear = inst.currentYear;
+  }
+  else {
+    var date = new Date();
+    inst.selectedDay = date.getDate();
+    inst.drawMonth = inst.selectedMonth = date.getMonth();
+    inst.drawYear = inst.selectedYear = date.getFullYear();
+    // the below two lines are new
+    this._setDateDatepicker(target, date);
+    this._selectDate(id, this._getDateDatepicker(target));
+  }
+  this._notifyChange(inst);
+  this._adjustDate(target);
+}
