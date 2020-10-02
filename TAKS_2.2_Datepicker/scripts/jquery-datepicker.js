cb8043b81d6582ec2f81c1ed1724fd43ae9019b6
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-01 21:19:33 -0700

    Task 2.2 - added standard JQuery datepicker with calendar icon (datepicker opens on both input focus or icon click); input and textarea borders styles now in separate css class (input, textarea)

---
+++ TAKS_2.2_Datepicker/scripts/jquery-datepicker.js
@@ -0,0 +1,8 @@
+$(function () {
+  $("#datepicker").datepicker({
+    showOn: "both",
+    buttonImage: "images/icons/calendar.gif",
+    buttonImageOnly: true,
+    buttonText: "Select date"
+  })
+});
