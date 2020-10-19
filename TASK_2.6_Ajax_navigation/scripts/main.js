abba134a94c0672d905c03219d72169e343ddc9b
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-19 01:43:52 -0700

    Task 2.6 - main.js refactored

--- TASK_2.6_Ajax_navigation/scripts/main.js
+++ TASK_2.6_Ajax_navigation/scripts/main.js
@@ -6,30 +6,19 @@ function initJQueryAjaxNavigation() {
   // on the initial page load, loading home page into the #main block
   $("#main").load("./pages/home.html");
 
-  // setting up page load on click and on focus + enter
-  $("#home").click(function() {
-    $("#main").load("./pages/home.html");
-  });
-  $("#home").on("keydown", function(e){
-    if(isEnterKey(e))
-      $("#main").load("./pages/home.html");
-  });
+  setUpAjaxLoadingOnClickOrEnterKey("#home", "#main", "./pages/home.html");
+  setUpAjaxLoadingOnClickOrEnterKey("#about", "#main", "./pages/about.html");
+  setUpAjaxLoadingOnClickOrEnterKey("#contact", "#main", "./pages/contact.html");
 
-  $("#about").click(function() {
-    $("#main").load("./pages/about.html");
-  });
-  $("#about").on("keydown", function(e){
-    if(isEnterKey(e))
-      $("#main").load("./pages/about.html");
-  });
-
-  $("#contact").click(function() {
-    $("#main").load("./pages/contact.html");
-  });
-  $("#contact").on("keydown", function(e){
-    if(isEnterKey(e))
-      $("#main").load("./pages/contact.html");
-  });
+  function setUpAjaxLoadingOnClickOrEnterKey(clickableElementSelector, loadIntoElementSelector, dataToLoad) {
+    $(clickableElementSelector).click(function() {
+      $(loadIntoElementSelector).load(dataToLoad);
+    });
+    $(clickableElementSelector).on("keydown", function(e){
+      if(isEnterKey(e))
+        $(loadIntoElementSelector).load(dataToLoad);
+    });
+  }
 
   function isEnterKey(event) {
     return event.keyCode === 13;
