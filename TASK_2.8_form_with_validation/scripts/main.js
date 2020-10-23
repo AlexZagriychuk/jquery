efd1b1fbb47ac55b8bde73c30b1815929a18d831
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-23 02:44:23 -0700

    Task 2.8 - implemented validation using jQuery for all inputs

--- TASK_2.8_form_with_validation/scripts/main.js
+++ TASK_2.8_form_with_validation/scripts/main.js
@@ -1,14 +1,37 @@
 $(function () {
+  initJQueryFormValidation();
   initJQueryAjaxPostFormDataToBackend();
 });
+
+function initJQueryFormValidation() {
+  $("#feedback-form").validate({
+    // validation rules
+    rules: {
+      name: "required",
+      email: {
+        required: true,
+        email: true
+      },
+      description: "required"
+    },
+    // validation error messages
+    messages: {
+      name: "Please enter your name",
+      email: "Please enter a valid email address",
+      description: "Please enter a description"
+    },
+  });
+}
 
 function initJQueryAjaxPostFormDataToBackend() {
   $("#feedback-form").submit(function (event) {
     // Stop form from submitting normally
     event.preventDefault();
 
-    $.post("./scripts/test.php", $(this).serialize())
-      .done(function (data) { console.log(data); })
-      .fail(function (error) { console.error(error); });
+    if ($(this).valid()) {
+      $.post("./scripts/test.php", $(this).serialize())
+        .done(function (data) { console.log(data); })
+        .fail(function (error) { console.error(error); });
+    }
   });
 }
