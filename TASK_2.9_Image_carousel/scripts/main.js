0147cee5b34d3deebbc8fa2ab85c4b0f8979f131
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-25 01:44:18 -0700

    Task 2.9 - website from TASK 1.5 (Bike shop)

---
+++ TASK_2.9_Image_carousel/scripts/main.js
@@ -0,0 +1,13 @@
+function toggleBurgerMenuBtn() {
+  let burger = document.getElementById('nav-burger-btn'),
+    activeClass = 'nav-burger__active';
+
+  burger && burger.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.currentTarget.classList.toggle(activeClass);
+  }, false);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  toggleBurgerMenuBtn();
+})
