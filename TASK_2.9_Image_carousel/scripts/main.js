26febb8287062bc93ee6d02b3c036748be2331ee
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-26 17:54:59 -0700

    Task 2.9 - fixed IE11 carousel width error; refactored the main.js file

--- TASK_2.9_Image_carousel/scripts/main.js
+++ TASK_2.9_Image_carousel/scripts/main.js
@@ -1,4 +1,9 @@
-function toggleBurgerMenuBtn() {
+document.addEventListener('DOMContentLoaded', function () {
+  initToggleBurgerMenuBtn();
+  initJQueryImageCarousel();
+})
+
+function initToggleBurgerMenuBtn() {
   let burger = document.getElementById('nav-burger-btn'),
     activeClass = 'nav-burger__active';
 
@@ -8,59 +13,49 @@ function toggleBurgerMenuBtn() {
   }, false);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  toggleBurgerMenuBtn();
-})
-
-$(function () {
-  initJQueryImageCarousel();
-});
-
 function initJQueryImageCarousel() {
-  var imgWrappers = $(".cards__img-wrapper");
-  $(imgWrappers).each(function (index) {
-    var imgWrapper = this;
-    imgWrapper.carouselImageIndex = 0;
-    imgWrapper.images = $(imgWrapper).find(".card__img-list li");
-    imgWrapper.radioButtons = $(imgWrapper).find(".card__img-nav-buttons input[type='radio']");
+  var imgCarousels = $(".cards__img-carousel");
+  $(imgCarousels).each(function () {
+    var imgCarousel = this;
+    imgCarousel.carouselImageIndex = 0;
+    imgCarousel.images = $(imgCarousel).find(".card__img-list li");
+    imgCarousel.radioButtons = $(imgCarousel).find(".card__img-nav-buttons input[type='radio']");
 
     // init left arrow listeners
-    var leftArrow = $(imgWrapper).find("*[id*='left_arrow']")[0];
-    leftArrow.imgWrapper = imgWrapper;
+    var leftArrow = $(imgCarousel).find(".left-arrow")[0];
     $(leftArrow).click(function () {
-      this.imgWrapper.updateCarouselImageIndex(-1);
-      this.imgWrapper.animateCarouselImage();
-      this.imgWrapper.updateCheckedRadioButton();
+      imgCarousel.updateCarouselImageIndex(-1);
+      imgCarousel.animateCarouselImage();
+      imgCarousel.updateCheckedRadioButton();
     });
 
     // init right arrow listeners
-    var rightArrow = $(imgWrapper).find("*[id*='right_arrow']")[0];
-    rightArrow.imgWrapper = imgWrapper;
+    var rightArrow = $(imgCarousel).find(".right-arrow")[0];
     $(rightArrow).click(function () {
-      this.imgWrapper.updateCarouselImageIndex(+1);
-      this.imgWrapper.animateCarouselImage();
-      this.imgWrapper.updateCheckedRadioButton();
+      imgCarousel.updateCarouselImageIndex(+1);
+      imgCarousel.animateCarouselImage();
+      imgCarousel.updateCheckedRadioButton();
     });
 
     // init radio buttons listeners
-    $(imgWrapper.radioButtons).each(function (radioIndex) {
-      this.imgWrapper = imgWrapper;
+    $(imgCarousel.radioButtons).each(function () {
       $(this).click(function () {
-        this.imgWrapper.carouselImageIndex = this.getClickedRadioButtonIndex();
-        this.imgWrapper.animateCarouselImage();
+        imgCarousel.carouselImageIndex = this.getClickedRadioButtonIndex();
+        imgCarousel.animateCarouselImage();
       });
 
       this.getClickedRadioButtonIndex = getClickedRadioButtonIndex;
     });
 
-    imgWrapper.updateCarouselImageIndex = updateCarouselImageIndex;
-    imgWrapper.animateCarouselImage = animateCarouselImage;
-    imgWrapper.updateCheckedRadioButton = updateCheckedRadioButton;
+    imgCarousel.updateCarouselImageIndex = updateCarouselImageIndex;
+    imgCarousel.animateCarouselImage = animateCarouselImage;
+    imgCarousel.updateCheckedRadioButton = updateCheckedRadioButton;
   });
 
   function updateCarouselImageIndex(diff) {
     this.carouselImageIndex += diff;
     var imagesLength = this.images.length;
+
     if (this.carouselImageIndex < 0) {
       this.carouselImageIndex += imagesLength;
     } else if (this.carouselImageIndex >= imagesLength) {
@@ -70,6 +65,7 @@ function initJQueryImageCarousel() {
 
   function animateCarouselImage() {
     var newMarginLeft = -100 * this.carouselImageIndex + '%';
+
     $(this.images[0]).animate({ marginLeft: newMarginLeft });
   }
 
@@ -79,9 +75,11 @@ function initJQueryImageCarousel() {
 
   function getClickedRadioButtonIndex() {
     var childrenOfParent = $(this).parent().children();
-    for(var i = 0; i < childrenOfParent.length; i++) {
-      if(childrenOfParent[i] === this) return i;
+
+    for (var i = 0; i < childrenOfParent.length; i++) {
+      if (childrenOfParent[i] === this) return i;
     }
+
     return -1;
   }
 }
