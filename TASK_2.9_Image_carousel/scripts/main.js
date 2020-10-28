75454b3b202fe128a64098a6e8089111344f08b8
Alex Zagriychuk <AlexZagriychuk@gmail.com>
2020-10-27 20:25:10 -0700

    Task 2.9 - carousel nav buttons are now generated dynamically in the main.js (instead of statically in the index.html)

--- TASK_2.9_Image_carousel/scripts/main.js
+++ TASK_2.9_Image_carousel/scripts/main.js
@@ -14,14 +14,17 @@ function initToggleBurgerMenuBtn() {
 }
 
 function initJQueryImageCarousel() {
-  var imgCarousels = $(".cards__img-carousel");
-  
+  var imgCarousels = $(".cards__img-carousel"),
+    checkedNavButtonClassName = "checked-img-nav-button";
+
   $(imgCarousels).each(function () {
-    var imgCarousel = this;
+    var imgCarousel = this,
+      imgNavButtonsClassName = "card__img-nav-buttons";
 
     imgCarousel.carouselImageIndex = 0;
     imgCarousel.images = $(imgCarousel).find(".card__img-list li");
-    imgCarousel.navButtons = $(imgCarousel).find(".card__img-nav-buttons li");
+    $(imgCarousel).append(generateNavButtonsListHTML(imgNavButtonsClassName, imgCarousel.images.length));
+    imgCarousel.navButtons = $(imgCarousel).find("." + imgNavButtonsClassName + " li");
 
     // init left and right arrow listeners
     $('.img-nav-arrow', imgCarousel).on('click', function (e) {
@@ -55,6 +58,20 @@ function initJQueryImageCarousel() {
     imgCarousel.updateCheckedNavButton = updateCheckedNavButton;
   });
 
+  function generateNavButtonsListHTML(listCssClass, listItemsCount) {
+    var html = '<ul class="' + listCssClass + '">';
+
+    for (var i = 0; i < listItemsCount; i++) {
+      if (i === 0) {
+        html += '<li class="' + checkedNavButtonClassName + '"></li>';
+      } else {
+        html += '<li></li>';
+      }
+    }
+
+    return html + "</ul>"
+  }
+
   function updateCarouselImageIndex(diff) {
     this.carouselImageIndex += diff;
     var imagesLength = this.images.length;
@@ -73,8 +90,7 @@ function initJQueryImageCarousel() {
   }
 
   function updateCheckedNavButton() {
-    var imgCarousel = this,
-      checkedNavButtonClassName = "checked-img-nav-button";
+    var imgCarousel = this;
 
     $(imgCarousel.navButtons).each(function (index) {
       var navButton = imgCarousel.navButtons[index];
